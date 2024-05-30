505466867447175fd05d6ba2274a39f7d988a8e3
Sammy-O <156414617+SamuelOmoding@users.noreply.github.com>
2024-05-30 10:17:08 +0300

    Delete sss_portfolio directory

--- sss_portfolio/src/assets/index.js
+++
@@ -1,9 +0,0 @@
-import blacklogo from './images/blacklogo.png';
-import whitelogo from './images/whitelogo.png';
-import redlogo from './images/redlogo.png';
-
-export {
-    redlogo,
-    blacklogo,
-    whitelogo,
-};
